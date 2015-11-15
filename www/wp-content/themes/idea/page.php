220ea730f237bf7d5096e317d71f75419f301529
vasiliy <krot1113@yandex.ru>
2015-11-15 21:57:11 +0400

    page and sidebar

--- www/wp-content/themes/idea/page.php
+++ www/wp-content/themes/idea/page.php
@@ -13,26 +13,25 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+<div class="container">
+	<?php get_sidebar()?>
+	<div id="right-column">
 		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+			// Start the loop.
+			while ( have_posts() ) : the_post();
 
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
+				// Include the page content template.
+				get_template_part( 'content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		// End the loop.
-		endwhile;
+			// End the loop.
+			endwhile;
 		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+	</div>
+</div>
 
 <?php get_footer(); ?>
--- www/wp-content/themes/idea/sidebar.php
+++ www/wp-content/themes/idea/sidebar.php
@@ -8,23 +8,123 @@
  */
 
 if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar( 'sidebar-1' )  ) : ?>
-	<div id="secondary" class="secondary">
+	<div id="left-column">
 
-		<?php if ( has_nav_menu( 'primary' ) ) : ?>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php
+		<!-- sub menu -->
+		<ul id="sub-menu">
+			<li>
+				<a class="outdoor-icon" href="#">Наружная реклама</a>
+				<ul>
+					<li><a href="#">Вывески</a></li>
+					<li><a href="#">Информационные стенды</a></li>
+					<li><a href="#">Объемные буквы</a></li>
+					<li><a href="#">Оформление мест продаж</a></li>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="print-icon" href="#">Широкоформатная печать</a>
+				<ul>
+					<li><a href="#">Вывески</a></li>
+					<li><a href="#">Информационные стенды</a></li>
+					<li><a href="#">Объемные буквы</a></li>
+					<li><a href="#">Оформление мест продаж</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="uv-icon" href="#">UV-печать</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="media-icon" href="#">Медиа-фасады, экраны</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="adv-icon" href="#">Размещение рекламы</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="flag-icon" href="#">Флаги, флагштоки</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="polygr-icon" href="#">Полиграфия</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="cloth-icon" href="#">Корпоративная одежда</a>
+				<ul>
+					<li><a href="#">Световые короба</a></li>
+					<li><a href="#">Таблички указатели</a></li>
+					<li><a href="#">Штендеры</a></li>
+				</ul>
+			</li>
+		</ul>
+
+		<!-- last news -->
+		<h2>Новости и акции</h2>
+		<div id="last-news">
+			<ul>
+				<li>
+					<h3>Новая акция</h3>
+					<div class="date">14.03.2015</div>
+					<div class="validity">с 18.05.2015 до 24.09.2015</div>
+					<p>Таким образом постоянное информационно-пропагандистское обеспечение нашей деятельности требуют определения и уточнения системы обучения кадров, соответствует насущным потребностям.</p>
+				</li>
+				<li>
+					<h3>Новая акция</h3>
+					<div class="date">14.03.2015</div>
+					<div class="validity">с 18.05.2015 до 24.09.2015</div>
+					<p>Постоянное информационно-пропагандистское обеспечение нашей деятельности требуют определения и уточнения системы обучения кадров, соответствует.</p>
+				</li>
+				<li>
+					<h3>Новая акция</h3>
+					<div class="date">14.03.2015</div>
+					<div class="validity">с 18.05.2015 до 24.09.2015</div>
+					<p>Товарищи, дальнейшее развитие различных форм деятельности играет важную роль в формировании систем массового участия.</p>
+				</li>
+			</ul>
+			<a href="#" class="btn btn-orange">Посмотреть все</a>
+		</div>
+		<div id="secondary" class="secondary" style="display:none;">
+
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<?php
 					// Primary navigation menu.
 					wp_nav_menu( array(
 						'menu_class'     => 'nav-menu',
 						'theme_location' => 'primary',
 					) );
-				?>
-			</nav><!-- .main-navigation -->
-		<?php endif; ?>
+					?>
+				</nav><!-- .main-navigation -->
+			<?php endif; ?>
 
-		<?php if ( has_nav_menu( 'social' ) ) : ?>
-			<nav id="social-navigation" class="social-navigation" role="navigation">
-				<?php
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav id="social-navigation" class="social-navigation" role="navigation">
+					<?php
 					// Social links navigation menu.
 					wp_nav_menu( array(
 						'theme_location' => 'social',
@@ -32,16 +132,18 @@ if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar(
 						'link_before'    => '<span class="screen-reader-text">',
 						'link_after'     => '</span>',
 					) );
-				?>
-			</nav><!-- .social-navigation -->
-		<?php endif; ?>
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
 
-		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-			<div id="widget-area" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-1' ); ?>
-			</div><!-- .widget-area -->
-		<?php endif; ?>
+			<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+				<div id="widget-area" class="widget-area" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-1' ); ?>
+				</div><!-- .widget-area -->
+			<?php endif; ?>
 
-	</div><!-- .secondary -->
+		</div><!-- .secondary -->
+	</div>
+
 
 <?php endif; ?>
