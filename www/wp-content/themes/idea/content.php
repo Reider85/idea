165de5b8969c323edf295483f35fc5adff939cd0
vasiliy <krot1113@yandex.ru>
2016-01-23 22:49:28 +0400

    news fix

--- www/wp-content/themes/idea/content.php
+++ www/wp-content/themes/idea/content.php
@@ -45,8 +45,5 @@
 	?>
 
 	<footer class="entry-footer">
-		<?php twentyfifteen_entry_meta(); ?>
 		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
-
-
